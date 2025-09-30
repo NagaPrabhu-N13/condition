84341bcd76446861224a1972308f45e86b5edce8
NagaPrabhu-N13 <nagaprabhu.n@creativityventures.co.in>
2025-09-30 20:14:03 +0530

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,8 +1,9 @@
 let num = 10;
-// Fix: Use the strict equality operator '===' for comparison
+
 if (num === 5) { 
   console.log("Five");
 } else {
   console.log("Not Five");
 }
-// Correct Output: "Not Five"
+
+
