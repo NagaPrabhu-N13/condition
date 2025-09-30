67b2e4087641995c68c36668b2368feecb78aa20
NagaPrabhu-N13 <nagaprabhu.n@creativityventures.co.in>
2025-09-30 20:12:11 +0530

    Add files via upload

---
+++ debug.js
@@ -0,0 +1,8 @@
+let num = 10;
+// Fix: Use the strict equality operator '===' for comparison
+if (num === 5) { 
+  console.log("Five");
+} else {
+  console.log("Not Five");
+}
+// Correct Output: "Not Five"
